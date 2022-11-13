4c08686f0c6b5e9ebba56e58c44943aab2c708b7
CristianGet90 <cristian.system22@gmail.com>
2022-11-13 12:42:16 -0500

    primer comit Angular

---
+++ FrontendPedidos/src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-identificacion',
+  templateUrl: './identificacion.component.html',
+  styleUrls: ['./identificacion.component.css']
+})
+export class IdentificacionComponent implements OnInit {
+
+  fgValidador: FormGroup = this.fb.group({
+    'usuario': ['', [Validators.required, Validators.email]],
+    'clave': ['', [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  IdentificarUsuario(){
+    let usuario = this.fgValidador.controls["usuario"].value;
+    let clave = this.fgValidador.controls["clave"].value;
+
+    alert(usuario + ":" + clave);
+  }
+
+}
